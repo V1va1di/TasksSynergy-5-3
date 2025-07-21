db11df09d5650461b2912eda628932d82df513f4
V1va1di <h0me6.9w0rk0@gmail.com>
2025-07-21 18:43:22 +0400

    Урок №5. Логические и условные операторы
    Задание №2
    
    Дано слово из маленьких латинских букв. Сколько там согласных и гласных букв? Гласными называют буквы «a», «e», «i», «o», «u».
    
    Для решения задачи создайте переменную и в неё положите слово с помощью input()
    
    А также определите количество каждой из этих гласных букв Если какой-то из перечисленных букв нет - Выведите False

---
+++ TasksSynergy 5-2.py
@@ -0,0 +1,21 @@
+word = (input("Введите слово: "))
+a = word.count('a')
+e = word.count('e')
+i = word.count('i')
+o = word.count('o')
+u = word.count('u')
+y = word.count('y')
+if a == 0:
+    print("a = False")
+if e == 0:
+    print("e = False")
+if i == 0:
+    print("i = False")
+if o == 0:
+    print("o = False")
+if u == 0:
+    print("u = False")
+if y == 0:
+    print("y = False")
+print(f"Гласных: {a + e + i + o + u}")
+print(f"Согласных: {len(word) - (a + e + i + o + u)}")
